6b691c851a6185059ff6f2f1c6a58568d97ed5b0
Kouhei <{mkkhiti.iltill@gmail.com}>
2023-03-20 21:58:20 +0900

    error message

--- src/components/common/button/LogoutButton.tsx
+++ src/components/common/button/LogoutButton.tsx
@@ -1,23 +1,31 @@
-import { FirebaseError } from "firebase/app";
 import { signOut } from "firebase/auth";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../../lib/firebase";
 import { CommonButton } from "../../../types/props/common/button";
 
 export function LogoutButton({ show }: CommonButton) {
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   const handleLogout = () => {
+    setError("");
     signOut(auth)
       .then(() => navigate("/"))
-      .catch((e: FirebaseError) => console.error(e.message));
+      .catch(() => setError("Logout failed for some reason."));
   };
   return (
-    <button
-      className={`${
-        !show ? "hidden" : ""
-      } absolute right-0 mr-3 w-[80px] py-3 border-[var(--font)] border-2`}
-      onClick={handleLogout}>
-      Logout
-    </button>
+    <>
+      {!!error ? (
+        <p>{error}</p>
+      ) : (
+        <button
+          className={`${
+            !show ? "hidden" : ""
+          } absolute right-0 mr-3 w-[80px] py-3 border-[var(--font)] border-2`}
+          onClick={handleLogout}>
+          Logout
+        </button>
+      )}
+    </>
   );
 }
